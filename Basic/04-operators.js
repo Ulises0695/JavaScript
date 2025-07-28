663c31607caacb2b7a4edc50ec600659296ff7fa
Ulises0695 <unidad7stp@gmail.com>
2025-07-27 20:08:12 -0600

    Segunda leccion de APERADORES

---
+++ Basic/04-operators.js
@@ -0,0 +1,48 @@
+// Operadores
+
+// Operadores Aritmeticos
+
+let a = 5
+let b = 10
+
+console.log(a + b)//Suma
+console.log(a - b )//Resta
+console.log(a * b )//Multiplicacion
+console.log(a / b ) //Division
+
+console.log(a % b ) //Modulo
+console.log(a ** b)//Exponente
+
+a++ //Incremento
+console.log(a)
+
+b-- //Decremento
+console.log(b)
+
+//Operadores de asignacion
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2
+console.log(myVariable)
+
+myVariable -= 2
+myVariable *= 2
+myVariable /= 2
+myVariable %= 2
+myVariable **= 2
+
+//Operadores de comparacion
+
+console.log(a)
+
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a == 6) // Igualdad por valor
+console.log(a == "6") //igualdad por valor
+console.log(a === a) //Igualdad por identidad (por tipo y valor)
+console.log(a === 6)
