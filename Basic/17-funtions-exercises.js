0bf02a76b57a0bbec115f8d490fb6dd104437339
Ulises0695 <ulisesutsem@gmail.com>
2026-03-13 17:41:54 -0600

    PRIMEROS DOS EJERCIOS DEL APARTADO 17

--- Basic/17-funtions-exercises.js
+++ Basic/17-funtions-exercises.js
@@ -7,10 +7,36 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 
 // 1. Crea una función que reciba dos números y devuelva su suma
 
+function sum(a, b){
+    console.log(a+b)
+}
+sum(30,40)
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+
+function numeroMayor(numeros) {
+  return Math.max(...numeros);
+}
+
+let resultado = numeroMayor([15, 54, 30, 22]);
+
+console.log(resultado);
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
+function contarVocales(texto) {
+  let contador = 0;
+  let vocales = "aeiouAEIOU";
+
+  for (let i = 0; i < texto.length; i++) {
+    if (vocales.includes(texto[i])) {
+      contador++;
+    }
+  }
+
+  return contador;
+}
+console.log(contador)
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
